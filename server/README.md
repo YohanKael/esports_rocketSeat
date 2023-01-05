cff693ea10c63b3ffd5ed6011a46364946ae54c4
YohanKael <yohan_garcia4@hotmail.com>
2023-01-05 16:33:20 -0300

    implementando front-end e back-end

---
+++ server/README.md
@@ -0,0 +1,29 @@
+# Back-end
+
+## Entidades
+
+### Game 
+
+id
+tittle
+bannerURL
+
+### AD
+
+Id
+gameId
+name
+yearsPlaying
+discord
+weekDays
+hourStart
+hourEnd
+useVoiceChannel
+creatAt
+
+## Casos de uso
+
+-Listagem de games com contagem de anúncios
+-Criação de novo anúncio
+-listagem de anuncios por game
+-Buscar discord pelo ID do anúncio
---
+++ server/prisma/migrations/20220930150800_seeds_table_games/migration.sql
@@ -0,0 +1,9 @@
+-- SEEDS FOR Game TABLE
+INSERT INTO "Game" ("id", "title", "bannerURL")
+VALUES
+("1", "CS:GO", "https://static-cdn.jtvnw.net/ttv-boxart/32399_IGDB-188x250.jpg"),
+("2", "CS 1.6", "https://static-cdn.jtvnw.net/ttv-boxart/10710_IGDB-285x380.jpg"),
+("3", "PUBG", "https://static-cdn.jtvnw.net/ttv-boxart/493057-285x380.jpg"),
+("4", "Valorant", "https://static-cdn.jtvnw.net/ttv-boxart/516575-285x380.jpg"),
+("5", "Apex Legends", "https://static-cdn.jtvnw.net/ttv-boxart/511224-285x380.jpg"),
+("6", "GTA V", "https://static-cdn.jtvnw.net/ttv-boxart/32982_IGDB-285x380.jpg")
